0cc211a4cd657759b73408c75d7cca261669be7d
sis-flag <57098249+sis-flag@users.noreply.github.com>
2022-09-18 16:01:03 +0800

    ppr failed

--- FVM code/ppr_point_temp.m
+++ FVM code/ppr_point_temp.m
@@ -1,10 +1,10 @@
-function du = ppr_continous_point(Mesh, PDE, ue)
+function du = ppr_point_temp(Mesh, PDE, ue)
 
 du = zeros(Mesh.nP, 2);
 
 for P = 1:Mesh.nP
-    xc = Mesh.xp(P); yc = Mesh.yp(P);
-    if xc < 1/4 || xc > 3/4 || yc < 1/4 || yc > 3/4
+    if Mesh.isbdp(P)
+        xc = Mesh.xp(P); yc = Mesh.yp(P);
         du(P,:) = PDE.du(xc, yc);
         continue
     end
@@ -17,14 +17,14 @@ for P = 1:Mesh.nP
         
     tx = Mesh.xe(nEs) - Mesh.xp(P);
     ty = Mesh.ye(nEs) - Mesh.yp(P);
-%     h = (mean(abs(tx)) + mean(abs(ty))) / 2;
-%     tx = tx/h; ty = ty/h;
 
     A = [ones(length(nEs),1), tx, ty, ...
         tx.*tx, tx.*ty, ty.*ty];
+
 %     A = [ones(length(nEs),1), tx, ty, ...
 %         tx.*tx, tx.*ty, ty.*ty, ...
 %         tx.*tx.*ty, tx.*ty.*ty, tx.*tx.*ty.*ty];
+    
     a = A \ ue(nEs);
 
     du(P,1) = a(2); du(P,2) = a(3);
--- FVM code/test_order_temp.m
+++ FVM code/test_order_temp.m
@@ -9,38 +9,6 @@ old_path = path(old_path, './utils');
 
 PDE = linear_stab1();
 
-% p = 0.0;
-% all_Mesh = cell(5,1);
-% all_Mesh{1} = get_rand_mesh(8, 8, p);
-% all_Mesh{2} = get_rand_mesh(16, 16, p);
-% all_Mesh{3} = get_rand_mesh(32, 32, p);
-% all_Mesh{4} = get_rand_mesh(64, 64, p);
-% all_Mesh{5} = get_rand_mesh(128, 128, p);
-
-% all_Mesh = cell(7,1);
-% load mesh/mesh11_1
-% all_Mesh{1} = arrange_polygonal(coord, U2P);
-% load mesh/mesh11_2
-% all_Mesh{2} = arrange_polygonal(coord, U2P);
-% load mesh/mesh11_3
-% all_Mesh{3} = arrange_polygonal(coord, U2P);
-% load mesh/mesh11_4
-% all_Mesh{4} = arrange_polygonal(coord, U2P);
-% load mesh/mesh11_5
-% all_Mesh{5} = arrange_polygonal(coord, U2P);
-% load mesh/mesh11_6
-% all_Mesh{6} = arrange_polygonal(coord, U2P);
-% load mesh/mesh11_7
-% all_Mesh{7} = arrange_polygonal(coord, U2P);
-
-% all_Mesh = cell(6,1);
-% all_Mesh{1} = load_mesh_file('mesh/mesh4_1_1.txt');
-% all_Mesh{2} = load_mesh_file('mesh/mesh4_1_2.txt');
-% all_Mesh{3} = load_mesh_file('mesh/mesh4_1_3.txt');
-% all_Mesh{4} = load_mesh_file('mesh/mesh4_1_4.txt');
-% all_Mesh{5} = load_mesh_file('mesh/mesh4_1_5.txt');
-% all_Mesh{6} = load_mesh_file('mesh/mesh4_1_6.txt');
-
 all_Mesh = cell(6,1);
 all_Mesh{1} = load_mesh_file('mesh/mesh1_1.txt');
 all_Mesh{2} = load_mesh_file('mesh/mesh1_2.txt');
@@ -67,52 +35,14 @@ all_Mesh{6} = load_mesh_file('mesh/mesh1_6.txt');
 % all_Mesh{5} = get_sin_mesh(64, 64, p);
 % all_Mesh{6} = get_sin_mesh(128, 128, p);
 
+gamma = 1;
 
-%% EBS1
-% fprintf('\nECS-I:\n')
-% fprintf('DOF \t err \t order \t ppr err \t order\n')
-% 
-% derr0 = 0;
-% err0 = 0;
-% nE0 = 0;
-% for k = 1:length(all_Mesh)
-%     Mesh = all_Mesh{k};
-%     
-%     u_exact = zeros(Mesh.nE, 1);
-%     for E = 1:Mesh.nE
-%         xc = Mesh.xe(E); yc = Mesh.ye(E);
-%         u_exact(E) = PDE.u(xc, yc);
-%     end
-%     
-%     du_exact = zeros(Mesh.nP, 2);
-%     for P = 1:Mesh.nP
-%         xc = Mesh.xp(P); yc = Mesh.yp(P);
-%         du_exact(P,:) = PDE.du(xc, yc);
-%     end
-%     
-%     [A, F] = ECS1(Mesh, PDE);
-%     u = A \ F;
-%     du = ppr_continous_point(Mesh, PDE, u);
-%     
-%     nE = Mesh.nE;
-%     err = max(abs(u - u_exact));
-%     derr = max(max(abs(du - du_exact)));
-%     fprintf('%d \t %.2e \t %.2f \t %.2e \t %.2f\n', ...
-%         nE, err, -2 * (log(err0)-log(err)) / (log(nE0)-log(nE)), ...
-%         derr, -2 * (log(derr0)-log(derr)) / (log(nE0)-log(nE)) ...
-%         );
-%     nE0 = nE; err0 = err; derr0 = derr;
-% end
-
-%% EBS2
-gamma = 3;
-
-fprintf('\nECS-II (\\gamma = %g):\n', gamma)
-fprintf('DOF \t err \t order \n')
-
-derr0 = 0;
-err0 = 0;
 nE0 = 0;
+de00 = 0;
+e10 = 0;
+de10 = 0;
+e20 = 0;
+de20 = 0;
 for k = 1:length(all_Mesh)
     Mesh = all_Mesh{k};
     
@@ -127,48 +57,36 @@ for k = 1:length(all_Mesh)
         xc = Mesh.xp(P); yc = Mesh.yp(P);
         du_exact(P,:) = PDE.du(xc, yc);
     end
+    
+    du0 = ppr_point_temp(Mesh, PDE, u_exact);
+    
+    [A, F] = ECS1(Mesh, PDE);
+    u1 = A \ F;
+    du1 = ppr_point_temp(Mesh, PDE, u1);
     
     [A, F] = ECS2(Mesh, PDE, gamma);
-    u = A \ F;
-    du = ppr_continous_point(Mesh, PDE, u);
+    u2 = A \ F;
+    du2 = ppr_point_temp(Mesh, PDE, u2);
     
     nE = Mesh.nE;
-    err = max(abs(u - u_exact));
-    derr = max(max(abs(du - du_exact)));
-    fprintf('%d \t %.2e \t %.2f \t %.2e \t %.2f\n', ...
-        nE, err, -2 * (log(err0)-log(err)) / (log(nE0)-log(nE)), ...
-        derr, -2 * (log(derr0)-log(derr)) / (log(nE0)-log(nE)) ...
+    de0 = max(max(abs(du0 - du_exact)));
+    e1 = max(abs(u1 - u_exact));
+    de1 = max(max(abs(du1 - du_exact)));
+    e2 = max(abs(u2 - u_exact));
+    de2 = max(max(abs(du2 - du_exact)));
+    
+    fprintf('%d & %.2e & %.2f & %.2e & %.2f & %.2e & %.2f \\\\\n', nE, ...
+        de0, -2 * (log(de00)-log(de0)) / (log(nE0)-log(nE)), ...
+        e1, -2 * (log(e10)-log(e1)) / (log(nE0)-log(nE)), ...
+        de1, -2 * (log(de10)-log(de1)) / (log(nE0)-log(nE)), ...
+        e2, -2 * (log(e20)-log(e2)) / (log(nE0)-log(nE)), ...
+        de2, -2 * (log(de20)-log(de2)) / (log(nE0)-log(nE)) ...
         );
-    nE0 = nE; err0 = err; derr0 = derr;
-end
-
-%% PPR
-fprintf('\nPPR:\n')
-fprintf('DOF \t err \t order \n')
-
-derr0 = 0;
-nE0 = 0;
-for k = 1:length(all_Mesh)
-    Mesh = all_Mesh{k};
     
-    u_exact = zeros(Mesh.nE, 1);
-    for E = 1:Mesh.nE
-        xc = Mesh.xe(E); yc = Mesh.ye(E);
-        u_exact(E) = PDE.u(xc, yc);
-    end
-    
-    du_exact = zeros(Mesh.nP, 2);
-    for P = 1:Mesh.nP
-        xc = Mesh.xp(P); yc = Mesh.yp(P);
-        du_exact(P,:) = PDE.du(xc, yc);
-    end
-    
-    du = ppr_continous_point(Mesh, PDE, u_exact);
-    
-    nE = Mesh.nE;
-    derr = max(max(abs(du - du_exact)));
-    fprintf('%d \t %.2e \t %.2f\n', ...
-        nE, derr, -2 * (log(derr0)-log(derr)) / (log(nE0)-log(nE)) ...
-        );
-    nE0 = nE; derr0 = derr;
+    nE0 = nE;
+    de00 = de0;
+    e10 = e1;
+    de10 = de1;
+    e20 = e2;
+    de20 = de2;
 end
